07f33b6b447429e7d1158482c8986a9974c0ba73
Nekmo <contacto@nekmo.com>
2016-05-15 04:36:39 +0200

    Included STATIC_ROOT.

--- nekmocom/settings/production.py
+++ nekmocom/settings/production.py
@@ -2,3 +2,7 @@ from .defaults import *
 
 SECRET_KEY = getattr(secrets, 'SECRET_KEY', SECRET_KEY)
 DATABASES = getattr(secrets, 'DATABASES', DATABASES)
+
+DEBUG = False
+ALLOWED_HOSTS = ['*']
+STATIC_ROOT = os.path.expanduser('~/Static')
