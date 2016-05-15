62b1efe2711c5b7b0b34a9c38211b2ecb477b6d6
Nekmo <contacto@nekmo.com>
2016-05-15 04:38:57 +0200

    Removed STATICFILES_DIRS

--- nekmocom/settings/defaults.py
+++ nekmocom/settings/defaults.py
@@ -73,9 +73,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
 STATIC_ROOT = os.path.join(DATA_DIR, 'static')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'nekmocom', 'static'),
-)
 SITE_ID = 1
 
 
