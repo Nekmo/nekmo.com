b3709546cd548072d212661e1efdede19a4754fa
Nekmo <contacto@nekmo.com>
2019-06-29 00:39:19 +0200

    Docker-compose.

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM python:3.5
+
+ENV PYTHONUNBUFFERED 1
+
+# RUN apt-get install debian-archive-keyring && apt-get update && apt-get dist-upgrade
+RUN apt-get update
+RUN apt-get install gettext -y
+WORKDIR /tmp
+COPY requirements.txt .
+COPY requirements-freeze.txt .
+RUN echo "Installing requirements"
+RUN pip install -r requirements.txt
+RUN mkdir -p /var/log/gunicorn
+
+ENV PYTHONPATH "/code:${PYTHONPATH}"
+WORKDIR /code
+ENTRYPOINT [ "/usr/local/bin/gunicorn", "-w", "18", "-b", "0.0.0.0:8000", "nekmocom.wsgi:application" ]
--- nekmocom/settings/production.py
+++ nekmocom/settings/production.py
@@ -1,34 +1,42 @@
 from .defaults import *
 
 SECRET_KEY = getattr(secrets, 'SECRET_KEY', SECRET_KEY)
-DATABASES = getattr(secrets, 'DATABASES', DATABASES)
 HOME = getattr(secrets, 'HOME', os.environ.get('HOME', '')).rstrip('/')
 
+DATABASES = {
+    'default': {
+        'CONN_MAX_AGE': 0,
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'nekmocom_postgres_1',
+        'NAME': os.getenv("POSTGRES_DB"),
+        'USER': os.getenv("POSTGRES_USER"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
+        'PORT': '',
+    }
+}
 
-def expand_home(path):
-    return '{}/{}'.format(HOME, path)
 
-
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
-STATIC_ROOT = expand_home('Static')
-MEDIA_ROOT = expand_home('Media')
+STATIC_ROOT = '/static/'
+MEDIA_ROOT = '/media/'
 
 
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '127.0.0.1:11211',
+        'LOCATION': 'nekmocom_memcached_1:11211',
     }
 }
 
 TEMPLATES[0]['OPTIONS']['loaders'] = [
     ('django.template.loaders.cached.Loader', TEMPLATES[0]['OPTIONS']['loaders']),
 ]
+LOGS_DIRECTORY = '/var/log/gunicorn'
 
 
 def logging(name):
-    if not os.path.lexists(expand_home('Log')):
+    if not os.path.lexists(LOGS_DIRECTORY):
         print('Logging is not available for {}!'.format(name))
         return
     return {
@@ -40,21 +48,27 @@ def logging(name):
                 'class': 'logging.handlers.RotatingFileHandler',
                 'maxBytes': 1024 * 1024 * 20,  # 20MB
                 'backupCount': 3,
-                'filename': expand_home('Log/{}.err'.format(name)),
+                'filename': os.path.join(LOGS_DIRECTORY, '{}.err'.format(name)),
             },
             'file-debug': {
                 'level': 'DEBUG',
                 'class': 'logging.handlers.RotatingFileHandler',
                 'maxBytes': 1024 * 1024 * 20,  # 20MB
                 'backupCount': 3,
-                'filename': expand_home('Log/{}.log'.format(name)),
+                'filename': os.path.join(LOGS_DIRECTORY, '{}.log'.format(name)),
             },
         },
         'loggers': {
             'django': {
-                'handlers': ['file-error'],
+                'handlers': ['file-error', 'file-debug'],
+                'level': 'DEBUG',
+                'propagate': True,
+            },
+            '': {
+                'handlers': ['file-error', 'file-debug'],
                 'level': 'DEBUG',
                 'propagate': True,
             },
         },
     }
+
